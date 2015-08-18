ColinIanKing/fnotifystat

commit bfc3d13254d415af25613a8b80eab37e5c6e103e

Re-order items in struct for better memory packing
Signed-off-by: Colin Ian King <colin.king@canonical.com>

fnotifystat.c
@@ -89,8 +89,8 @@ typedef struct pathname_t {
 
 /* scaling factor */
 typedef struct {
-	const char ch;				/* Scaling suffix */
 	const uint64_t scale;			/* Amount to scale by */
+	const char ch;				/* Scaling suffix */
 } scale_t;
 
 /* stashed context for verbose info */

commit 20c718944b365d25d3d71cb6741d7a03345a864a

Switch fields back in scale_t
Fix regression caused by commit bfc3d13254d415af25613a8b80eab37e5c6e103e

Signed-off-by: Colin Ian King <colin.king@canonical.com>

fnotifystat.c
@@ -89,8 +89,8 @@ typedef struct pathname_t {
 
 /* scaling factor */
 typedef struct {
-	const uint64_t scale;			/* Amount to scale by */
 	const char ch;				/* Scaling suffix */
+	const uint64_t scale;			/* Amount to scale by */
 } scale_t;
 
 /* stashed context for verbose info */